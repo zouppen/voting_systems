d1d49b5ead8e18b62cc49eb0eca337aacba5d572
Joel Lehtonen <joel.lehtonen@iki.fi>
2009-09-30 20:01:19 +0300

    This thing can now count single votes. The terminology is made more clear.

--- ZouppenVoting.hs
+++ ZouppenVoting.hs
@@ -1,23 +1,37 @@
 module ZouppenVoting where
 
--- A single vote. The first argument is a ballot for the best candidate and the
--- second one is for the worst.
+import qualified Data.Map as Map
+
+-- Some terminology:
+---
+-- A ballot paper consists of two votes; one for the best and one for the worst
+-- candidate as seen by the voter.
+
+
+
+-- A single ballot paper. The first argument is a vote for the best candidate
+-- and the second one is for the worst.
 -- Map requires a vote to be ordered (Ord) so Equality (Eq) is not enough.
-data Ord a => Vote a = Vote {
+data Ord a => Ballot a = Ballot {
       best  :: a,
       worst :: a
     } deriving Show
 
 -- Trivial vote
 -- Takes a list of votes and returns the winner
-countVotes :: Ord t => [Vote t] -> t
-countVotes (x:xs) = best x
+--countVotes :: Ord t => [Vote t] -> t
+--countVotes (x:xs) = best x
 
 -- Checks if a vote is valid and contains real candidates
-checkVote :: Ord t => [t] -> (Vote t) -> Bool
+checkVote :: Ord t => [t] -> (Ballot t) -> Bool
 checkVote candidates vote
     | best vote == worst vote = False
-    | otherwise = and [best `elem` candidates, worst `elem` candidates]
+    | otherwise = and [(best vote) `elem` candidates,
+                       (worst vote) `elem` candidates]
 
 
-          
+countVotes :: Ord t => [t] -> Map.Map t Integer
+countVotes votes = foldr insertVote Map.empty votes
+    where insertVote vote prev = Map.insertWith (+) vote 1 prev 
+
+--countBestVotes votes = coun
