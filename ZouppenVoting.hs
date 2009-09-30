79efe1a6bc1586483591304b150a302df4bbea26
Joel Lehtonen <joel.lehtonen@iki.fi>
2009-09-30 18:17:18 +0300

    Tietorakenne toimii paremmin.

--- ZouppenVoting.hs
+++ ZouppenVoting.hs
@@ -1,12 +1,23 @@
 module ZouppenVoting where
 
--- A single vote
-data Vote a b = Vote a b deriving Show
+-- A single vote. The first argument is a ballot for the best candidate and the
+-- second one is for the worst.
+-- Map requires a vote to be ordered (Ord) so Equality (Eq) is not enough.
+data Ord a => Vote a = Vote a a deriving Show
 
 best (Vote a _) = a 
 worst (Vote _ a) = a 
 
 -- Trivial vote
 -- Takes a list of votes and returns the winner
-countVotes :: [Vote t t] -> t
+countVotes :: Ord t => [Vote t] -> t
 countVotes (x:xs) = best x
+
+-- Checks if a vote is valid and contains real candidates
+checkVote :: Ord t => [t] -> (Vote t) -> Bool
+checkVote candidates vote
+    | best vote == worst vote = False
+    | otherwise = True
+
+
+          
